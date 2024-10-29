514c0dc522b607de04f05b360631de87b96e1ab0
VinchC <vinch.cm@gmail.com>
2024-10-29 11:42:00 +0100

    SearchForm and SearchFormReset components created to manager query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
-export default function Home() {
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const queryB = (await searchParams).query;
+
   return (
     <>
       <section className="blue_container">
         <h1 className="heading">Tu n&apos;aimes pas les maths ?</h1>
         <p className="sub-heading !max-w-3xl">Tu es au bon endroit !</p>
+        <SearchForm query={queryB} />
       </section>
     </>
   );
