15b0ff33120a4121422a12da0d5e419b076ac1c4
VinchC <vinch.cm@gmail.com>
2024-10-29 08:45:22 +0100

    sign in and sign out implemented

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
+import { auth, signIn, signOut } from "@/auth";
 
-const Navbar = () => {
+// is user logged in ?
+
+const Navbar = async () => {
+  const session = await auth();
+
   return (
-    <header className="px-5 py-3 bg-white shadwo-smfont-work-sans">
+    <header className="px-5 py-3 bg-white shadwo-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
           <Image
@@ -13,6 +18,36 @@ const Navbar = () => {
             height={64}
           />
         </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/products/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut();
+                }}
+              >
+                <button type="submit">Sign out</button>
+              </form>
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Sign in</button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
