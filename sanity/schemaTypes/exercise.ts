27facb6d11d84cc912fbdcee8955892c590fd0e6
VinchC <vinch.cm@gmail.com>
2024-10-30 16:01:31 +0100

    three schemaTypes created, layout, sanity config, structure ans index updated accordingly

---
+++ sanity/schemaTypes/exercise.ts
@@ -0,0 +1,48 @@
+import { defineType, defineField } from "sanity";
+
+export const exercise = defineType({
+  name: "exercise",
+  title: "Exercise",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+    defineField({
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
+    }),
+    defineField({
+      name: "views",
+      type: "number",
+    }),
+    defineField({
+      name: "description",
+      type: "text",
+    }),
+    defineField({
+      name: "category",
+      type: "string",
+      validation: (Rule) =>
+        Rule.min(1).max(20).required().error("Merci d'entrer une catégorie"),
+    }),
+    defineField({
+      name: "picture",
+      type: "url",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "pitch",
+      type: "markdown",
+    }),
+  ],
+});
--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 
+import { author } from "@/sanity/schemaTypes/author";
+import { exercise } from './exercise';
+
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author, exercise],
 }
