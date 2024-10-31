58c10cf1c3880f7a9aa0675518dd3d657e443357
VinchC <vinch.cm@gmail.com>
2024-10-31 09:24:58 +0100

    Exercise query done, to be fetched

---
+++ sanity/lib/queries.ts
@@ -0,0 +1,19 @@
+import { defineQuery } from "next/sanity";
+
+export const EXERCISES_QUERY = defineQuery(
+  `*[_type == "exercise" && defined(slug.current)] | order(_createdAt desc) { 
+        _createdAt,
+        views,
+        author -> { 
+          _id,
+          name,
+          portrait
+        },
+        _id,
+        description,
+          picture,
+        category,
+        title,
+      }
+`
+);
