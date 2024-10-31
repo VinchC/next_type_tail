dbb6c85aba528ed1c5df6a7f121337ed48a27aae
VinchC <vinch.cm@gmail.com>
2024-10-31 16:10:54 +0100

    SkipLink added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,14 @@
 import Navbar from "../../components/Navbar";
+import SkipLink from "../../components/SkipLink";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <>
+    <SkipLink />
       <Navbar />
-      <main className="font-work-sans"> {children}</main>
+      <main id="maincontent" className="font-work-sans"> {children}</main>
     </>
   );
 }
---
+++ components/SkipLink.tsx
@@ -0,0 +1,7 @@
+export default function SkipLink() {
+    return (
+        <a href="#maincontent" className="skip">
+        Skip to main content
+      </a>
+    )
+}
